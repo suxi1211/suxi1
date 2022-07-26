437b9a5e4a77233b698998bc3ebf4d3f112b09fa
sh1211 <109769006+sh1211@users.noreply.github.com>
2022-07-25 21:43:44 -0400

    Update ForkingHomeworkRfile (2).R

--- ForkingHomeworkRfile (2).R
+++ ForkingHomeworkRfile (2).R
@@ -10,3 +10,5 @@ diamonds%>%
   filter(price > 7000 & price < 10000)%>%
   arrange(desc(carat))%>%
   slice(1:20)
+
+diamonds[, .(carat, cut, price)][cut == "Premium", price >7000 & price< 1000][order(-carat)][1:20]
